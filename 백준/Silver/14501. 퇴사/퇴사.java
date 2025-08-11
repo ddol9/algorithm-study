bb1357dee1b17298ca102e18db0c75494b3bd881
Yewon Choi <118425946+ddol9@users.noreply.github.com>
2025-08-11 15:48:51 +0900

    [Silver III] Title: 퇴사, Time: 68 ms, Memory: 11572 KB -BaekjoonHub

---
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
+        int[] time = new int[N];
+        int[] pay = new int[N];
+        int[] dp = new int[N + 1];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            time[i] = Integer.parseInt(st.nextToken());
+            pay[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (i > 0) {
+                dp[i + 1] = Math.max(dp[i + 1], dp[i]);
+            }
+            if (i + time[i] <= N) {
+                dp[i + time[i]] = Math.max(dp[i + time[i]], dp[i] + pay[i]);
+            }
+        }
+        System.out.println(dp[N]);
+    }
+}
