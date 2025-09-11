1fd147f2d617bb396defa9e089365209fad7fd44
Yewon Choi <118425946+ddol9@users.noreply.github.com>
2025-09-11 09:44:29 +0900

    [Gold IV] Title: 플로이드, Time: 420 ms, Memory: 45620 KB -BaekjoonHub

---
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+
+        int[][] d = new int[n + 1][n + 1];
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < n + 1; j++) {
+                d[i][j] = Integer.MAX_VALUE;
+            }
+            d[i][i] = 0;
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int r = Integer.parseInt(st.nextToken());
+
+            if (d[a][b] > r) d[a][b] = r;
+        }
+
+        for (int k = 1; k <= n; k++) {
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    if (d[i][k] == Integer.MAX_VALUE || d[k][j] == Integer.MAX_VALUE) continue;
+                    if (d[i][k] + d[k][j] < d[i][j]) {
+                        d[i][j] = d[i][k] + d[k][j];
+                    }
+                }
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (d[i][j] == Integer.MAX_VALUE) d[i][j] = 0;
+                System.out.print(d[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
